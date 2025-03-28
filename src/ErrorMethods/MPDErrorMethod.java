a2145c2e0f6dd252916ea958bf1a098585a5ef61
Barru Adi <barru.adi@gmail.com>
2025-03-28 22:31:20 +0700

    feat: MPD error method calc

--- src/ErrorMethods/MPDErrorMethod.java
+++ src/ErrorMethods/MPDErrorMethod.java
@@ -1,10 +1,48 @@
 package ErrorMethods;
 
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+
 public class MPDErrorMethod implements _ErrorMethod {
     @Override
-    public double calculateErr(int x, int y, int width, int height) {
-        // TODO: MPD LOGIC
+    public double calculateErr(BufferedImage image, int x, int y, int width, int height) {
+        //---------------Initialisasi------------------
+        long total_pixels = width * height;
+        int[] probability_R = new int[256]; double entropy_R = 0;
+        int[] probability_G = new int[256]; double entropy_G = 0;
+        int[] probability_B = new int[256]; double entropy_B = 0;
+
+        //---------------Hitung probabilitas------------------
+        for (int i = x; i < x + width; i++) {
+            for (int j = y; j < y + height; j++) {
+                Color color = new Color(image.getRGB(i, j));
+                probability_R[color.getRed()]++;
+                probability_G[color.getGreen()]++;
+                probability_B[color.getBlue()]++;
+            }
+        }
+
         
-        return 0;
+        //---------------Hitung Entropi--------------------
+        for (int i = 0; i < 256; i++) {
+            if (probability_R[i] > 0) {
+                double p = (double) probability_R[i] / total_pixels;
+                entropy_R -= p * Math.log(p) / Math.log(2);
+            }
+            if (probability_G[i] > 0) {
+                double p = (double) probability_G[i] / total_pixels;
+                entropy_G -= p * Math.log(p) / Math.log(2);
+            }
+            if (probability_B[i] > 0) {
+                double p = (double) probability_B[i] / total_pixels;
+                entropy_B -= p * Math.log(p) / Math.log(2);
+            }
+        }
+
+        
+        //---------------Hitung Entropi Total--------------------
+        double total_entropi = (entropy_R + entropy_G + entropy_B) / 3.0;
+        
+        return total_entropi;
     }
 }
