3b6c557e60cca3086f63b17dad686cf82fc2eb72
Barru Adi <barru.adi@gmail.com>
2025-03-29 07:33:51 +0700

    feat: main algorithm

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,46 @@
 // IMPORT ALL FILES
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+
 import Business.*;
-import ErrorMethods.*;
 import Utils.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Utils.clearScreen();
+        try {
+            // ------------- iNPUT -------------
+            CLIio cli = new CLIio();
+            cli.cliInput();
+            
+            
+            // ------------- PROSES -------------
+            BufferedImage image = cli.getImage();
+            ImageTree tree = new ImageTree(
+                image, 
+                cli.getInputThreshold(), 
+                cli.getInputBlockSize(), 
+                cli.getInputMethod()
+            );
+
+            BufferedImage outputImage = new BufferedImage(
+                image.getWidth(), 
+                image.getHeight(), 
+                BufferedImage.TYPE_INT_RGB
+            );
+            tree.reconstructImage(outputImage);
+
+            
+            // ------------- OUTPUT -------------
+            ImageIO.write(outputImage, "png", new File(cli.getOutputPath()));
+
+        } catch (IOException e) {
+            System.out.println("ERR:" + e);
+            return;
+        }
+
     }
 }
